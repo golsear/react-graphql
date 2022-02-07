07266e48de68d3440b54f9b3f77f1b2f1d482ed7
Serhii Holub <serhii.holub@valtech.com>
2022-02-07 12:46:27 +0200

    Resolving with Data

--- server.js
+++ server.js
@@ -1,32 +1,18 @@
 const express = require('express');
 //const expressGraphQL = require('express-graphql');
-var { graphqlHTTP } = require('express-graphql');
-var { buildSchema } = require('graphql');
-
-// Construct a schema, using GraphQL schema language
-/* var schema = buildSchema(`
-  type Query {
-    hello: String
-  }
-`); */
-
-// The root provides a resolver function for each API endpoint
-/* var root = {
-    hello: () => {
-      return 'Hello world!';
-    },
-  }; */
-
+const { graphqlHTTP } = require('express-graphql');
+//var { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
 
 const app = express();
 
 /* app.use('graphql', expressGraphQL({
+    schema: schema,
     graphiql: true
 })); */
 
 app.use('/graphql', graphqlHTTP({
-    //schema: schema,
-    //rootValue: root,
+    schema: schema,
     graphiql: true,
   }));
 
